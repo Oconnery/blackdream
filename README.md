4969085fa1883e6e70aa46c0cef75c3bdbc652a2
Oisin Connery <oisinconnery@hotmail.co.uk>
2023-10-17 14:23:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# blackdream
+
+Developed with Unreal Engine 5
---
+++ Source/blackdream/blackdream.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "blackdream.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, blackdream, "blackdream" );
